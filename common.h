4f94e14fb7a45074f568c40abb49dcfaaa8d9398
Scott Livingston <slivingston@caltech.edu>
2012-04-02 17:57:06 -0700

    Bump version for development, given new patching code.

--- common.h
+++ common.h
@@ -2,7 +2,7 @@
  * \brief Project-wide definitions and macros.
  *
  *
- * SCL; Mar 2012.
+ * SCL; 2012.
  */
 
 
@@ -10,7 +10,7 @@
 #define _COMMON_H_
 
 
-#define GR1C_VERSION "0.1b"
+#define GR1C_VERSION "0.1c"
 #define GR1C_COPYRIGHT "Copyright (c) 2012 by Scott C. Livingston, California Institute of Technology\nThis is free software, released under the GNU GPLv3 and without warranty."
 
 
