15423bd39098f6e67ecb4616c53d47d334f65d97
Scott Livingston <slivingston@caltech.edu>
2012-12-03 13:34:13 -0800

    manual for v0.4l

--- search/functions_74.js
+++ search/functions_74.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['tree_5fdot_5fdump',['tree_dot_dump',['../ptree_8h.html#a8632c58da19ad428dabd36e8c5325c1e',1,'ptree.c']]],
   ['tree_5fsize',['tree_size',['../ptree_8h.html#a30878b132f25dc77a40f9a62e98729bb',1,'ptree.c']]],
+  ['tulip0_5faut_5fdump',['tulip0_aut_dump',['../automaton_8h.html#a67ce928c0c55d6482bc6b6a0e7135240',1,'automaton_io.c']]],
   ['tulip_5faut_5fdump',['tulip_aut_dump',['../automaton_8h.html#ad135af09f28e95ad66d501893df662d6',1,'automaton_io.c']]]
 ];
