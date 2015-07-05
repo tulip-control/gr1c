44dec0d1939fb55f13a9c8b5a33040f6f4d86481
Scott Livingston <slivingston@cds.caltech.edu>
2015-07-05 09:08:37 -0700

    BUILD: SHA 256 instead of SHA-1 to check CUDD tarball

--- get-deps.sh
+++ get-deps.sh
@@ -3,19 +3,19 @@
 # Fetch dependencies, and place them in location that Makefile expects
 
 CUDDVER=2.5.0
-SHA1SUM=7d0d8b4b03f5c1819fe77a82f3b947421a72d629
+SHA256SUM=4f3bc49b35564af94b17135d8cb8c4063fb049cfaa442f80dc40ba73c6345a85
 URI=ftp://vlsi.colorado.edu/pub/cudd-$CUDDVER.tar.gz
 
 mkdir -p extern
 curl $URI > extern/cudd-$CUDDVER.tar.gz
 
-FILECHECKSUM=`sha1sum extern/cudd-$CUDDVER.tar.gz| sed 's/ .*//'`
-if [ "$SHA1SUM" = "$FILECHECKSUM" ]
+FILECHECKSUM=`sha256sum extern/cudd-$CUDDVER.tar.gz| sed 's/ .*//'`
+if [ "$SHA256SUM" = "$FILECHECKSUM" ]
 then
     cd extern
     tar -xzf cudd-$CUDDVER.tar.gz
 else
-    echo "Fetched file ($URI) has unexpected SHA-1 checksum."
+    echo "Fetched file ($URI) has unexpected SHA checksum."
     false
 fi
 echo "Successfully fetched CUDD; ready to build!"
