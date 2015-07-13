471391fadfd3aede7ed8941507409e1cb30b3d80
Scott Livingston <slivingston@cds.caltech.edu>
2015-07-13 13:12:25 -0700

    BUILD: Make curl silent except upon error
    as in get-deps.sh. This behavior should be automatic given that
    response data are to stdout and then redirected, but for completeness
    these command-line switches are added.
    
    [skip ci]

--- get-deps.sh
+++ get-deps.sh
@@ -7,7 +7,7 @@ SHA256SUM=4f3bc49b35564af94b17135d8cb8c4063fb049cfaa442f80dc40ba73c6345a85
 URI=ftp://vlsi.colorado.edu/pub/cudd-$CUDDVER.tar.gz
 
 mkdir -p extern
-curl $URI > extern/cudd-$CUDDVER.tar.gz
+curl -sS $URI > extern/cudd-$CUDDVER.tar.gz
 
 FILECHECKSUM=`sha256sum extern/cudd-$CUDDVER.tar.gz| sed 's/ .*//'`
 if [ "$SHA256SUM" = "$FILECHECKSUM" ]
