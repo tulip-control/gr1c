e855d664d41c5ca49a22301c5e81d95cca80e027
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-08-06 05:32:18 -0400

    Include check for and building of Spin in build-deps.sh
    If the Spin sources directory is found in the same location as it
    would be placed by get-extra-deps.sh, then build it.

--- build-deps.sh
+++ build-deps.sh
@@ -1,10 +1,18 @@
 #!/bin/sh -e
 #
 # Build dependencies locally under extern/
+# Also build extras if present, e.g., after get-extra-deps.sh
 
 CUDDVER=3.0.0
+SPINVER=6.4.5
 
 cd extern/src/cudd-$CUDDVER
 ./configure --prefix=`pwd`/../..
 make
 make install
+
+
+if [ -d ../Spin/Src${SPINVER} ]; then
+    cd ../Spin/Src${SPINVER}
+    make
+fi
