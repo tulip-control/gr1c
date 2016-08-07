3437d6c6002145a5f2aa6959f3b81f774e8b7fde
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-08-07 19:37:48 -0400

    TEST: Verify strategy for tests/specs/free_counter.spc

--- tests/test-verification.sh
+++ tests/test-verification.sh
@@ -18,3 +18,12 @@ if test $VERBOSE -eq 1; then
     echo
     ${SPINEXE} -V
 fi
+
+$BUILD_ROOT/gr1c -t aut specs/free_counter.spc > free_counter.spc.aut
+$BUILD_ROOT/gr1c-autman -i specs/free_counter.spc free_counter.spc.aut -P > free_counter.spc.aut.pml
+${SPINEXE} -f '!(X sysinit && [] (!checkstrans || systrans) && []<> sysgoal0000 && [] !pmlfault)' >> free_counter.spc.aut.pml
+${SPINEXE} -a free_counter.spc.aut.pml
+cc -o pan pan.c
+if test `./pan -a | grep errors| cut -d: -f2` -ne 0; then
+    exit 1
+fi
