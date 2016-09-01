tulip-control/gr1c

commit 016be4bca15a430e8149e2fc3c3bdbdc8bc983e8

Free memory to avoid leaks
In the current implementation, the case of the leaks from main() is
not important because the memory would be recovered anyway after
program termination.

The leak from check_realizable_internal() occurs as part of returning
with error after deciding that a parameter has an unexpected value.
Though not demonstrated in the current implementation, we can imagine
future designs or other uses of code from gr1c where this leak matters.

Both were found by the Clang static analyzer.

src/main.c
@@ -68,16 +68,16 @@ int main( int argc, char **argv )
     char **command_argv = NULL;
 
     byte verification_model = 0;  /* For command-line flag "-P". */
-    ptree_t *original_env_init;
-    ptree_t *original_sys_init;
-    ptree_t **original_env_goals;
-    ptree_t **original_sys_goals;
-    int original_num_egoals;
-    int original_num_sgoals;
-    ptree_t **original_env_trans_array;
-    ptree_t **original_sys_trans_array;
-    int original_et_array_len;
-    int original_st_array_len;
+    ptree_t *original_env_init = NULL;
+    ptree_t *original_sys_init = NULL;
+    ptree_t **original_env_goals = NULL;
+    ptree_t **original_sys_goals = NULL;
+    int original_num_egoals = 0;
+    int original_num_sgoals = 0;
+    ptree_t **original_env_trans_array = NULL;
+    ptree_t **original_sys_trans_array = NULL;
+    int original_et_array_len = 0;
+    int original_st_array_len = 0;
 
     int i, j, var_index;
     ptree_t *tmppt;  /* General purpose temporary ptree pointer */
@@ -496,9 +496,17 @@ int main( int argc, char **argv )
                             &spc.sys_trans_array, &spc.st_array_len,
                             &spc.env_goals, spc.num_egoals, &spc.sys_goals, spc.num_sgoals,
                             init_flags, verbose ) < 0) {
+        for (j = 0; j < original_num_egoals; j++)
+            free( *(original_env_goals+j) );
         free( original_env_goals );
+        for (j = 0; j < original_num_sgoals; j++)
+            free( *(original_sys_goals+j) );
         free( original_sys_goals );
+        for (j = 0; j < original_et_array_len; j++)
+            free( *(original_env_trans_array+j) );
         free( original_env_trans_array );
+        for (j = 0; j < original_st_array_len; j++)
+            free( *(original_sys_trans_array+j) );
         free( original_sys_trans_array );
         return -1;
     }

src/solve.c
@@ -915,6 +915,7 @@ DdNode *check_realizable_internal( DdManager *manager, DdNode *W,
 
     } else {
         fprintf( stderr, "Error: Unrecognized init_flags %d", init_flags );
+        free( cube );
         return NULL;
     }
 

commit 96e1d9ed5b27fef8b1ddd38235890e99c4776349

Avoid dead free() calls if expand_nonbool_GR1() fails
Additional free() calls were introduced in commits
016be4bca15a430e8149e2fc3c3bdbdc8bc983e8
and
e58b6853248aefcb87cc7d5aa89eca7b04932a47
from main(), so the leaks were not known to be significant.
However, the relevant variables are only used if verification_model > 0.
In this changeset, the corresponding free() calls are thus moved to
a conditional block under verification_model > 0.

src/main.c
@@ -496,18 +496,20 @@ int main( int argc, char **argv )
                             &spc.sys_trans_array, &spc.st_array_len,
                             &spc.env_goals, spc.num_egoals, &spc.sys_goals, spc.num_sgoals,
                             init_flags, verbose ) < 0) {
-        for (j = 0; j < original_num_egoals; j++)
-            free( *(original_env_goals+j) );
-        free( original_env_goals );
-        for (j = 0; j < original_num_sgoals; j++)
-            free( *(original_sys_goals+j) );
-        free( original_sys_goals );
-        for (j = 0; j < original_et_array_len; j++)
-            free( *(original_env_trans_array+j) );
-        free( original_env_trans_array );
-        for (j = 0; j < original_st_array_len; j++)
-            free( *(original_sys_trans_array+j) );
-        free( original_sys_trans_array );
+        if (verification_model > 0) {
+            for (j = 0; j < original_num_egoals; j++)
+                free( *(original_env_goals+j) );
+            free( original_env_goals );
+            for (j = 0; j < original_num_sgoals; j++)
+                free( *(original_sys_goals+j) );
+            free( original_sys_goals );
+            for (j = 0; j < original_et_array_len; j++)
+                free( *(original_env_trans_array+j) );
+            free( original_env_trans_array );
+            for (j = 0; j < original_st_array_len; j++)
+                free( *(original_sys_trans_array+j) );
+            free( original_sys_trans_array );
+        }
         return -1;
     }
     spc.nonbool_var_list = expand_nonbool_variables( &spc.evar_list, &spc.svar_list,