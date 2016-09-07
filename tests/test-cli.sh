b03b611bc02a9b479ff945e3798b0ec5c4b4f815
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-09-07 04:37:05 -0400

    Add test cases that involve non-existing files
    To do this, a format is introduced in test-cli.sh to check for already
    existing files in the tests/ directory.
    
    TEST CASE: gr1c -- rg
    
    which is motivated because `gr1c rg` is valid (the `rg` command),
    whereas `gr1c -- rg` is for a file named "rg".
    
    TEST CASE: gr1c f00f
    
    should fail because the file named "f00f" does not exist.

--- tests/test-cli.sh
+++ tests/test-cli.sh
@@ -18,9 +18,24 @@ export PATH=${BUILD_ROOT}:$PATH
 if test $VERBOSE -eq 1; then
     echo '\nChecking for detection of flawed command-line usage...'
 fi
-for args in '-notoption' '-hh' '--00f'; do
+
+# Format for each test case: ARGS[;FILE]
+#
+# where the command is `gr1c ARGS` and if FILE is given, then the test
+# case fails if the file is found to already exist.  E.g., the
+# optional FILE is motivated to avoid naming collisions.
+for args in '-notoption' '-hh' '--00f' '-- rg  # where `rg` is not a file.;rg' 'f00f  # attempt to read from file that does not exist.;f00f'; do
     if test $VERBOSE -eq 1; then
-        echo "\t gr1c ${args}"
+        echo "\t gr1c ${args%%;*}"
+    fi
+    file=${args##*;}
+    args=${args%%;*}
+    if [ -n "${file}" ]; then
+        if [ -r ${file} ]; then
+            echo $PREFACE 'Local file `'${file}'` found in tests directory. It will interfere with tests.'
+            echo
+            exit 1
+        fi
     fi
     gr1c ${args} > /dev/null 2>&1
     if [ $? -eq 0 ]; then
