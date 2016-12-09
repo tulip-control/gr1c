70d22f24048fde4c1eada353a666240b89f00ae7
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-12-09 00:34:19 -0500

    TEST: Test more aspects of return value from var_to_bool

--- tests/test_bitblasting.c
+++ tests/test_bitblasting.c
@@ -5,6 +5,7 @@
 
 #define _POSIX_C_SOURCE 200809L
 #include <stdlib.h>
+#include <string.h>
 
 #include "ptree.h"
 #include "tests_common.h"
@@ -17,6 +18,17 @@ int main( int argc, char **argv )
     head = var_to_bool( "x", 2 );
     if (head == NULL) {
         ERRPRINT( "var_to_bool() unexpectedly returned NULL (error)." );
+        abort();
+    }
+    if (tree_size( head ) != 2) {
+        ERRPRINT1( "var_to_bool() returned list of %d variables, "
+                   "rather than list of length 2.",
+                   tree_size( head ) );
+        abort();
+    }
+    if (strncmp( "x0", (get_list_item( head, 0 ))->name, 2 )
+        || strncmp( "x1", (get_list_item( head, 1 ))->name, 2 )) {
+        ERRPRINT( "unexpected bit-index variable name from var_to_bool()" );
         abort();
     }
 
