5a5516b23e9022ab5fac5dc70ba8d46d0c550600
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-05-01 06:31:57 -0400

    Only fetch dependency tar file if one not found
    get-deps.sh will download the CUDD distribution tarball only if a file
    of the same name does not already exist locally. In any case, the SHA
    hash of the local file is checked. This change is motivated by
    avoiding unnecessary network activity and accidental deletion of your
    local copy of the file if `curl` fails.
    
    Changes and motivations for get-extra-deps.sh are entirely similar.

--- get-deps.sh
+++ get-deps.sh
@@ -7,7 +7,10 @@ SHA256SUM=b8e966b4562c96a03e7fbea239729587d7b395d53cadcc39a7203b49cf7eeb69
 URI=ftp://vlsi.colorado.edu/pub/cudd-$CUDDVER.tar.gz
 
 mkdir -p extern/src
-curl -sS $URI > extern/cudd-$CUDDVER.tar.gz
+if [ ! -f extern/cudd-$CUDDVER.tar.gz ]
+then
+    curl -sS $URI > extern/cudd-$CUDDVER.tar.gz
+fi
 
 if hash sha256sum >/dev/null 2>&1; then
     FILECHECKSUM=`sha256sum extern/cudd-$CUDDVER.tar.gz| sed 's/ .*//'`
--- get-extra-deps.sh
+++ get-extra-deps.sh
@@ -8,7 +8,10 @@ SHA256SUM=44081282eb63cd9df763ebbcf8bad19dbeefecbebf8ac2cc090ea92e2ab71875
 URI=http://spinroot.com/spin/Src/spin${SPINVER}.tar.gz
 
 mkdir -p extern/src
-curl -sS $URI > extern/spin${SPINVER}.tar.gz
+if [ ! -f extern/spin${SPINVER}.tar.gz ]
+then
+    curl -sS $URI > extern/spin${SPINVER}.tar.gz
+fi
 
 if hash sha256sum >/dev/null 2>&1; then
     FILECHECKSUM=`sha256sum extern/spin${SPINVER}.tar.gz| sed 's/ .*//'`
