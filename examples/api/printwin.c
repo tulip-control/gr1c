deec9d8809b38d50e3e78d38bd61164fe4ed889a
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-05-03 23:56:48 -0400

    Create skeleton of first example of using API

---
+++ examples/api/printwin.c
@@ -0,0 +1,18 @@
+/* Enumerate states in winning set.
+ *
+ * An example of using the gr1c API. Provide the specification by invoking with
+ * a file name. It prints states with variable ordering as in the specification.
+ *
+ *
+ * SCL; 2017
+ */
+
+
+#include <stdio.h>
+
+
+int main( int argc, char **argv )
+{
+
+    return 0;
+}
