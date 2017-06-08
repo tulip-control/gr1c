a29e35fcdaeefa40d8eac514f451c9da8d9a808e
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-06-08 00:11:19 -0700

    TEST: Add case of two successors for aut_aut_dumpver()

--- tests/test_aut_aut_dump.c
+++ tests/test_aut_aut_dump.c
@@ -5,6 +5,7 @@
 
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
@@ -22,6 +23,12 @@
 "1\n" \
 "0 1 0 0 1 0 -1 1\n" \
 "1 0 0 0 1 0 -1 1\n"
+
+#define REF_GR1CAUT_SINGLE_LOOP_AND_2SUCC \
+"1\n" \
+"0 0 1 0 1 0 -1 0 2\n" \
+"1 1 0 0 1 0 -1 2\n" \
+"2 0 0 0 1 0 -1 2\n"
 
 
 #define STRING_MAXLEN 2048
@@ -116,6 +123,57 @@ int main(void)
         ERRPRINT( "output of aut_aut_dump does not match expectation" );
         ERRPRINT1( "%s", instr  );
         ERRPRINT1( "%s", REF_GR1CAUT_SINGLE_LOOP_AND_SUCC );
+        abort();
+    }
+
+    state[0] = 0;
+    state[1] = 1;
+    state[2] = 0;
+    head = insert_anode( head, 0, -1, True, state, state_len );
+    assert( head != NULL );
+
+    this_node = find_anode( head, 0, state, state_len );
+    assert( this_node != NULL );
+
+    state[1] = 0;
+    that_node = find_anode( head, 0, state, state_len );
+    assert( that_node != NULL );
+
+    this_node->trans = malloc( 2*sizeof(anode_t *) );
+    assert( this_node->trans );
+    this_node->trans_len = 2;
+    *(this_node->trans) = this_node;
+    *(this_node->trans+1) = that_node;
+
+    if (fseek( fp, 0, SEEK_SET )) {
+        perror( __FILE__ ", fseek" );
+        abort();
+    }
+    if (ftruncate( fd, 0 )) {
+        perror( __FILE__ ", ftruncate" );
+        abort();
+    }
+
+    assert( !aut_aut_dumpver( head, state_len, fp, 1 ) );
+
+    if (fseek( fp, 0, SEEK_SET )) {
+        perror( __FILE__ ", fseek" );
+        abort();
+    }
+
+    /* NB, assumed width may cause problems if we start using Unicode. */
+    if (fread( instr, sizeof(char), strlen(REF_GR1CAUT_SINGLE_LOOP_AND_2SUCC), fp )
+        < strlen(REF_GR1CAUT_SINGLE_LOOP_AND_2SUCC)) {
+        ERRPRINT( "output of aut_aut_dump is too short." );
+        ERRPRINT1( "%s", instr  );
+        ERRPRINT1( "%s", REF_GR1CAUT_SINGLE_LOOP_AND_2SUCC );
+        abort();
+    }
+    if (strncmp( instr, REF_GR1CAUT_SINGLE_LOOP_AND_2SUCC,
+                 strlen(REF_GR1CAUT_SINGLE_LOOP_AND_2SUCC) )) {
+        ERRPRINT( "output of aut_aut_dump does not match expectation" );
+        ERRPRINT1( "%s", instr  );
+        ERRPRINT1( "%s", REF_GR1CAUT_SINGLE_LOOP_AND_2SUCC );
         abort();
     }
 
