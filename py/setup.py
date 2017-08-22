cd26a3a43737e54698009da1438d96b73981b49d
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-08-22 16:36:58 -0700

    Add Python 3.6 as target supported client platform
    [ci skip]

--- py/setup.py
+++ py/setup.py
@@ -29,5 +29,6 @@ setup(name='gr1c',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5']
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6']
       )
