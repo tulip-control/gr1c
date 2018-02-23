8c779cfd43c3659d3f047e7d0630b8ac88eb4aaa
Scott C. Livingston <slivingston@cds.caltech.edu>
2018-02-23 14:07:48 -0800

    BUILD: improve generality of get-deps.sh

--- get-deps.sh
+++ get-deps.sh
@@ -4,7 +4,7 @@
 
 CUDDVER=3.0.0
 SHA256SUM=b8e966b4562c96a03e7fbea239729587d7b395d53cadcc39a7203b49cf7eeb69
-URI=https://sourceforge.net/projects/cudd-mirror/files/cudd-3.0.0.tar.gz/download
+URI=https://sourceforge.net/projects/cudd-mirror/files/cudd-$CUDDVER.tar.gz/download
 #URI=ftp://vlsi.colorado.edu/pub/cudd-$CUDDVER.tar.gz
 
 mkdir -p extern/src
